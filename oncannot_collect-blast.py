f2e3f1af3343b10ec2020f0119208397726e51b1
bvreede <b.vreede@gmail.com>
2014-04-22 17:16:16 +0300

    save to outputfile

--- oncannot_collect-blast.py
+++ oncannot_collect-blast.py
@@ -1,7 +1,9 @@
 import csv, urllib2, re
 
 inputdb = "segmentation_bicoid.csv"
+
 genelist = csv.reader(open(inputdb))
+output = open("%s-output.csv" %(inputdb[:-4]),"w")
 
 ### collect gene and protein sequences and save ###
 for gene in genelist:
@@ -14,8 +16,8 @@ for gene in genelist:
 	region_xml = urllib2.urlopen(region_url)
 	exons = []
 	pre_exon = ""
-	exoff = 0
-	# read xml document: find CDS by colour
+	exoff = 0 #collecting exon: yes (1) or no (0)
+	# parse xml document: find CDS by colour
 	for line in region_xml:
 		linecheck = 0 #to ensure that lines are not added in duplicate
 		if line.find('#E0FFFF">')!=-1:
@@ -37,11 +39,20 @@ for gene in genelist:
 				pre_exon += line.strip()
 				linecheck = 1
 	if len(exons)!=0: #prevents crash in case the gene gives an error
+		CDSlist = []
 		for each in exons:
 			exon = re.split('</span><span style="Background-color: #E0FFFF; Color: #0000F.">', each) #removes html info that signifies exon
 			exon2 = "".join(exon[1:]).split('</span><span style="Background-color: #E0FFFF">') # removes intron-before-exon, joins the list, splits off the intron-after-exon
-			exon_final = exon2[0]
-			print exon_final
+			CDSlist.append(exon2[0])		
+		totalgene = "".join(CDSlist)
+		print totalgene
+		output.write("%s,%s,whole,%s\n" %(fbid,genename,totalgene)) #entry with whole genome info
+		for i in range(len(CDSlist)):
+			print i
+		CDSlist = []
+	else:
+		output.write("%s,%s,error\n" %(fbid,genename)) #entry when gene gives an error
+	
 
 
 	transl_url = "http://flybase.org/cgi-bin/getseq.html?source=dmel&id=%s&chr=3L&dump=PrecompiledFasta&targetset=translation" %fbid
