68b3ea820976a5811202ac20def4a4389d7162aa
Barbara Vreede <b.vreede@gmail.com>
2014-05-26 23:42:39 +0300

    added report statements

--- annotation.sh
+++ annotation.sh
@@ -4,7 +4,7 @@ exon=${1%.csv}"-exons.csv"
 blast=${1%.csv}"-blast.csv"
 
 python exonfinder_prot.py $1
-echo "All exons found! Moving on to blasting stage..."
+echo "Done finding exons! Moving on to blasting stage..."
 python exonblast.py $exon $2
-echo "All exons blasted! Now to align them..."
+echo "Done blasting exons! Now to align them..."
 python blastalign.py $blast $2
