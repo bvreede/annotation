acba352bdbb9c30375eb7261bb2f91940336c3bc
Barbara Vreede <b.vreede@gmail.com>
2014-05-27 14:58:25 +0300

    added time statement

--- annotation.sh
+++ annotation.sh
@@ -3,8 +3,8 @@
 exon=${1%.csv}"-exons.csv"
 blast=${1%.csv}"-blast.csv"
 
-python exonfinder_prot.py $1
+time python exonfinder_prot.py $1
 echo "Done finding exons! Moving on to blasting stage..."
-python exonblast.py $exon $2
+time python exonblast.py $exon $2
 echo "Done blasting exons! Now to align them..."
-python blastalign.py $blast $2
+time python blastalign.py $blast $2
