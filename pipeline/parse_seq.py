4192d8499519feab5c1a290c4695cf78ef08b667
Barbara Vreede <b.vreede@gmail.com>
2014-08-18 03:13:41 +0300

    input parameters and verification

---
+++ pipeline/parse_seq.py
@@ -0,0 +1,51 @@
+'''
+This script can be used in a pipeline to connect the parsed info
+from a blast output with the sequence retriever (getseq.py).
+The script requires customization of the genome file location.
+The script works with NCBI blast 2.2.29+ in python 2.7.
+Author: Barbara Vreede
+Contact: b.vreede@gmail.com
+Date: 18 August 2014
+'''
+
+import sys,os
+
+'''
+CUSTOMIZE THE FOLLOWING PATHS!
+To allow easy switching to different genomes, add any new genomes used
+to the 'genomedict' dictionary instead of replacing the existing information.
+'''
+genomedir = "/home/barbara/data/genomes/" #directory where genomes are located
+genomedict = {"Ofas":"Ofasciatus/Ofas.scaffolds.fa", "Clec":"Clectularius/Clec_Bbug02212013.genome.fa"} #name of blastable genome dbs
+
+'''
+Specifying input parameters
+'''
+if len(sys.argv) <= 2:
+	sys.exit("USAGE: python parse_seq.py species infolder")
+
+species = sys.argv[1]
+infolder = sys.argv[2]
+
+'''
+Verifying input parameters
+'''
+if species in genomedict:
+	genome = genomedir + genomedict[species]
+else: 
+	sys.exit("Species genome not found. Add the path to the getseq.py script, or check your spelling. (e.g. 'Ofas'.)")
+if os.path.exists(genome):
+	readgenome = open(genome)
+else:
+	sys.exit("Could not find genome directory. Verify path in getseq.py code.")
+if infolder[-1:] == "/": #to prevent mishaps when folder name is specified with / at the end
+	infolder = infolder[:-1]
+if os.path.exists(infolder):
+	continue
+else:
+	sys.exit("Could not find input directory. Verify path in input parameters.")
+
+
+
+for filename in os.listdir(infolder):
+	if 
